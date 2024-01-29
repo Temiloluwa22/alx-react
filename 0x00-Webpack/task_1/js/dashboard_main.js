f52ab58c4b67f0a210f677115bc2039f769aa387
Temiloluwa22 <113702411+Temiloluwa22@users.noreply.github.com>
2024-01-29 17:22:19 +0100

    Create dashboard_main.js

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,5 @@
+Holberton Dashboard
+Paragraph element: Dashboard data for the students
+Button element with the text Click here to get started
+Add a paragraph element as so <p id='count'></p>
+ Copyright - Holberton School
