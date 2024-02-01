b0ad5a41e50b8c6d186cd495c56e066e0726a2d2
Temiloluwa22 <113702411+Temiloluwa22@users.noreply.github.com>
2024-02-01 16:24:31 +0100

    Add files via upload

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,17 @@
-Holberton Dashboard
-Paragraph element: Dashboard data for the students
-Button element with the text Click here to get started
-Add a paragraph element as so <p id='count'></p>
- Copyright - Holberton School
+import $ from "jquery";
+import _ from "lodash";
+
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append('<p id="count"></p>');
+$("body").append("<p>Copyright - Holberton School</p>");
+
+let conunt = 0;
+
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
